2b52ce1b9f24870eaf10b0c145983f1be39e0913
Michael Riccelli <michaelriccelli@ons-fvff82l0q6lr.myfiosgateway.com>
2022-10-17 16:55:34 -0400

    All home slider styles and html along with javascript

--- js/script.js
+++ js/script.js
@@ -20,4 +20,13 @@ document.querySelector('#close-search').addEventListener('click', (event) => {
     
 window.onscroll = () => {
     searchBar.classList.remove('active');
-}
+};
+
+var swiper = new Swiper(".home-slider", {
+    loop: true,
+    grabCursor: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
