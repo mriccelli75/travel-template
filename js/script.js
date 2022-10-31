5dda486da9b11f3340a156ff4bdf31e07367db76
Michael Riccelli <michaelriccelli@ons-fvff82l0q6lr.myfiosgateway.com>
2022-10-31 07:16:32 -0400

    all blogs html css and javascript

--- js/script.js
+++ js/script.js
@@ -90,3 +90,24 @@ var swiper = new Swiper(".home-slider", {
         }
       },
   });
+
+  var swiper = new Swiper(".blogs-slider", {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        991: {
+          slidesPerView: 3,
+        }
+      },
+  });
